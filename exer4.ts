43248e23851477ac1eaeee27d15f45bb50989280
khailu <kalebgebre@gmail.com>
2017-04-17 16:53:02 -0500

    //typescript

---
+++ exer4.ts
@@ -0,0 +1,14 @@
+class Person{
+Constractor(firstname : String){
+this.firstname = firstname;
+}
+get firstname(){
+return this.firstname;
+}
+set firstname(value : String){
+this firstname = value.toUppercase();
+}
+
+Person.ferstname = ‘’Asaad”;
+console.log(Person.firstname);
+}
